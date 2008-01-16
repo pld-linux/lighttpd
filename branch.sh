51e3e0fc5cda23e7208e926bf8fe20025bcd778d
Elan Ruusamäe <glen@pld-linux.org>
2008-01-16 15:59:39 +0000

    - filter out configure.in and SConstruct
    Changed files:
        branch.sh -> 1.2

--- branch.sh
+++ branch.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-svn=svn://svn.lighttpd.net/lighttpd/
+set -e
+svn=svn://svn.lighttpd.net/lighttpd
 tag=lighttpd-1.4.18
 branch=lighttpd-1.4.x
-svn diff --old=$svn/tags/$tag --new=$svn/branches/$branch > lighttpd-branch.diff
+
+old=$svn/tags/$tag
+new=$svn/branches/$branch
+echo "Running diff: $old -> $new"
+LC_ALL=C svn diff --old=$old --new=$new > lighttpd-branch.diff
+
+echo "exclude files which change version"
+filterdiff -x 'configure.in' -x 'SConstruct' lighttpd-branch.diff > lighttpd-branch.diff.tmp
+mv -f lighttpd-branch.diff.tmp lighttpd-branch.diff
