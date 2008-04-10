ff26356a6a836701554b36a885294ccb29adee81
Elan Ruusamäe <glen@pld-linux.org>
2008-04-10 18:53:36 +0000

    - for 1.4.19 diffs
    Changed files:
        branch.sh -> 1.3

--- branch.sh
+++ branch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 svn=svn://svn.lighttpd.net/lighttpd
-tag=lighttpd-1.4.18
+tag=lighttpd-1.4.19
 branch=lighttpd-1.4.x
 
 old=$svn/tags/$tag
@@ -9,6 +9,6 @@ new=$svn/branches/$branch
 echo "Running diff: $old -> $new"
 LC_ALL=C svn diff --old=$old --new=$new > lighttpd-branch.diff
 
-echo "exclude files which change version"
-filterdiff -x 'configure.in' -x 'SConstruct' lighttpd-branch.diff > lighttpd-branch.diff.tmp
+echo "Excluding files which change version (configure.in, SConstruct)"
+filterdiff -x 'configure.in' lighttpd-branch.diff > lighttpd-branch.diff.tmp
 mv -f lighttpd-branch.diff.tmp lighttpd-branch.diff
