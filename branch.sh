7ec65614b6c827e0f9c95841ce8ebcf887b337f2
Elan Ruusamäe <glen@pld-linux.org>
2008-06-17 16:10:03 +0000

    - ignore .cvsignore from branch.diff
    Changed files:
        branch.sh -> 1.4
        lighttpd-branch.diff -> 1.36

--- branch.sh
+++ branch.sh
@@ -9,6 +9,6 @@ new=$svn/branches/$branch
 echo "Running diff: $old -> $new"
 LC_ALL=C svn diff --old=$old --new=$new > lighttpd-branch.diff
 
-echo "Excluding files which change version (configure.in, SConstruct)"
-filterdiff -x 'configure.in' lighttpd-branch.diff > lighttpd-branch.diff.tmp
+echo "Excluding files which change version or were not in dist tarball"
+filterdiff -x 'configure.in' -x .cvsignore lighttpd-branch.diff > lighttpd-branch.diff.tmp
 mv -f lighttpd-branch.diff.tmp lighttpd-branch.diff
