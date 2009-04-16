0e60c194c0478e496a89d202224cdf0856f4c34c
Elan Ruusamäe <glen@pld-linux.org>
2009-04-16 21:52:57 +0000

    - exclude files that were not present in tarball
    Changed files:
        branch.sh -> 1.10
        lighttpd-branch.diff -> 1.50

--- branch.sh
+++ branch.sh
@@ -7,8 +7,9 @@ branch=lighttpd-1.4.x
 old=$svn/tags/$tag
 new=$svn/branches/$branch
 echo "Running diff: $old -> $new"
-LC_ALL=C svn diff --old=$old --new=$new > lighttpd-branch.diff
+LC_ALL=C svn diff --old=$old --new=$new > lighttpd-branch.diff.tmp
 
 echo "Excluding files which change version or were not in dist tarball"
-filterdiff -x 'configure.in' -x 'SConstruct' -x 'CMakeLists.txt' lighttpd-branch.diff > lighttpd-branch.diff.tmp
-mv -f lighttpd-branch.diff.tmp lighttpd-branch.diff
+filterdiff -x 'configure.in' -x 'SConstruct' -x 'CMakeLists.txt' -x 'src/CMakeLists.txt' -x 'src/config.h.cmake' -x 'src/mod_uploadprogress.c' lighttpd-branch.diff.tmp > lighttpd-branch.diff.tmp2
+mv -f lighttpd-branch.diff.tmp2 lighttpd-branch.diff
+rm -f lighttpd-branch.diff.tmp
