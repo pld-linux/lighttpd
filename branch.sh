cc37bbfb7095c9610d1754476eac01a5eae78664
Elan Ruusamäe <glen@pld-linux.org>
2009-09-21 13:23:45 +0000

    - remove revno-s for smaller diffs
    Changed files:
        branch.sh -> 1.13
        lighttpd-branch.diff -> 1.58

--- branch.sh
+++ branch.sh
@@ -13,3 +13,6 @@ echo "Excluding files which change version or were not in dist tarball"
 filterdiff -x tests/mod-extforward.conf -x ChangeLog -x .cvsignore -x src/.cvsignore -x tests/.cvsignore -x doc/.cvsignore -x 'configure.in' -x 'SConstruct' -x 'CMakeLists.txt' -x 'src/CMakeLists.txt' -x 'src/config.h.cmake' -x 'src/mod_uploadprogress.c' lighttpd-branch.diff.tmp > lighttpd-branch.diff.tmp2
 mv -f lighttpd-branch.diff.tmp2 lighttpd-branch.diff
 rm -f lighttpd-branch.diff.tmp
+
+# remove revno's for smaller diffs
+sed -i -e 's,^\([-+]\{3\} .*\)\t(revision [0-9]\+)$,\1,' lighttpd-branch.diff
