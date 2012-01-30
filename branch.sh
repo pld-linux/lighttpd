cc782f0fa90cbbd8f930da768f97811945afb5ad
cvs2git <feedback@pld-linux.org>
2012-01-30 18:49:32 +0000

    This commit was manufactured by cvs2git to create branch 'DEVEL'.
    Cherrypick from master 2009-01-19 10:54:50 UTC Elan Ruusamäe <glen@pld-linux.org> '- new':
        branch.sh -> 1.6
        lighttpd-mod_proxy_backend_ajp13.conf -> 1.1
        lighttpd-mod_proxy_backend_fastcgi.conf -> 1.1
        lighttpd-mod_proxy_backend_http.conf -> 1.1
        lighttpd-mod_proxy_backend_scgi.conf -> 1.1
    Cherrypick from master 2012-01-30 18:49:32 UTC Jan Rękorajski <baggins@pld-linux.org> '- add systemd tmpfiles config':
        lighttpd.conf -> 1.49
        lighttpd.init -> 1.19
        lighttpd.tmpfiles -> 1.1
        lighttpd.upstart -> 1.1

---
+++ branch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+svn=svn://svn.lighttpd.net/lighttpd
+tag=lighttpd-1.4.20
+branch=lighttpd-1.4.x
+
+old=$svn/tags/$tag
+new=$svn/branches/$branch
+echo "Running diff: $old -> $new"
+LC_ALL=C svn diff --old=$old --new=$new > lighttpd-branch.diff
+
+echo "Excluding files which change version or were not in dist tarball"
+filterdiff -x 'configure.in' lighttpd-branch.diff > lighttpd-branch.diff.tmp
+mv -f lighttpd-branch.diff.tmp lighttpd-branch.diff
