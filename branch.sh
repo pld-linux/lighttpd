1af77202404e89a6363a4392be7df16b12dac54c
Elan Ruusamäe <glen@delfi.ee>
2016-09-02 00:06:47 +0300

    branch.sh: fetch refs properly

--- branch.sh
+++ branch.sh
@@ -37,7 +37,7 @@ if [ ! -d $repo ]; then
 fi
 
 cd $repo
-	git fetch
+	git fetch origin +$branch:$branch +refs/tags/$tag:refs/tags/$tag
 	git log -p --reverse $tag..$branch | filter > ../$out.tmp
 cd ..
 
